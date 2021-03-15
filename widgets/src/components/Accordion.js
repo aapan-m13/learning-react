c842df509b4d10044d2fe15f9442291549f83e34
am13 <aapan.cuet.cse@gmail.com>
2021-03-16 01:38:42 +0600

    css link add widget

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,36 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Accordion = ({items}) =>{
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
+  const onTitleClicked = (index) => {
+    console.log(activeIndex);
+    if (activeIndex != null) setActiveIndex(null);
+    else setActiveIndex(index);
+  };
 
-    const [activeIndex, setActiveIndex] = useState(null);
-
-    const onTitleClicked = (index)=>{
-        setActiveIndex(index);
-    }
-
-    const renderedItems = items.map((item, index)=>{
-
-        const active = index === activeIndex ? 'active': '';
-        
-        return (
-            <React.Fragment key={item.title}>
-                <div className={`title ${active}`} onClick = {()=>onTitleClicked(index)} >
-                    <i className='dropdown icon'></i>
-                    {item.title}
-                </div>
-                <div className={`content ${active}`}>
-                    <p>{item.content}</p>
-                </div>
-            </React.Fragment>
-        )
-    })
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
 
     return (
-        <div className='ui styled accordion'>
-            {renderedItems}
+      <React.Fragment key={item.title}>
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClicked(index)}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
-    )
-}
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
 export default Accordion;
