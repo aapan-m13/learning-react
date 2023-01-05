f737cda4eb226e5e52f401819e3fb5a395b643f0
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-01-05 18:24:12 +0600

    props

--- course 2/react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
+++ course 2/react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
+  console.log(props.data);
   return (
     <div className="expense-item">
-      <div>31 March, 2023</div>
+      <ExpenseDate date={props.data.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">100.00</div>
+        <h2>{props.data.title}</h2>
+        <div className="expense-item__price">{`$${props.data.amount}`}</div>
       </div>
     </div>
   );
