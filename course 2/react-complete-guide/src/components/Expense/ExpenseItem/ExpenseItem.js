280d4f8e9da5296a9a2a01997142559de8d13e70
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-01-23 01:05:32 +0600

    use state

--- course 2/react-complete-guide/src/components/Expense/ExpenseItem/ExpenseItem.js
+++ course 2/react-complete-guide/src/components/Expense/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
 
 export default function ExpenseItem(props) {
-  // console.log(props.data);
+  const [title, setTitle] = useState(props.data.title);
+
+  const changeTitle = () => {
+    setTitle("updated");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.data.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{`$${props.data.amount}`}</div>
+        <button onClick={changeTitle}>Change Title</button>
       </div>
     </Card>
   );
--- course 2/react-complete-guide/src/components/UI/Card/Card.js
+++ course 2/react-complete-guide/src/components/UI/Card/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./card.css";
 
-export default function (props) {
-  console.log(props);
+export default function Card(props) {
   return <div className={"card " + props.className}>{props.children}</div>;
 }
