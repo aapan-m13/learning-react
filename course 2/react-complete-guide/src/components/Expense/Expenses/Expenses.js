585bbac0247b2be6df103c87f389124f1b4ff06a
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-02-05 20:51:38 +0600

    assignment

--- course 2/react-complete-guide/src/components/Expense/Expenses/Expenses.js
+++ course 2/react-complete-guide/src/components/Expense/Expenses/Expenses.js
@@ -1,9 +1,18 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 import React from "react";
 
 export default function Expenses(props) {
+  const yearSelectionHandler = (selectedData) => {
+    console.log(`from expenses, ${selectedData}`);
+  };
   const expenses = props.expenseData.map((elm) => {
-    return <ExpenseItem data={elm} key={elm.id}></ExpenseItem>;
+    return (
+      <div>
+        <ExpensesFilter selectedYear={yearSelectionHandler}></ExpensesFilter>
+        <ExpenseItem data={elm} key={elm.id}></ExpenseItem>;
+      </div>
+    );
   });
 
   return <div>{expenses}</div>;
