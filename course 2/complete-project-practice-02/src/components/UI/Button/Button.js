46345e6d206d0285aad60314f21446efe39d075d
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-03-20 21:15:05 +0600

    project button add and styling

---
+++ course 2/complete-project-practice-02/src/components/UI/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Button.module.css";
+export default function Button(props) {
+  return (
+    <button
+      className={styles["button"]}
+      type={props.type ? props.type : "button"}
+    >
+      {props.children}
+    </button>
+  );
+}
