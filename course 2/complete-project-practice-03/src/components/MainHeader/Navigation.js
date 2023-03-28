45d6c89226297a08822faffc6f055d3a2f1f320e
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-03-28 20:29:24 +0600

    new proj

---
+++ course 2/complete-project-practice-03/src/components/MainHeader/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import classes from "./Navigation.module.css";
+
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <React.Fragment>
+            <li>
+              <a href="/">Users</a>
+            </li>
+            <li>
+              <a href="/">Admin</a>
+            </li>
+            <li>
+              <button onClick={props.onLogout}>Logout</button>
+            </li>
+          </React.Fragment>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
