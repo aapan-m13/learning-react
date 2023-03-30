222fb63e8ef032a9b4f2c0e702a9b91c56012079
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-03-30 23:02:57 +0600

    useeffect

--- course 2/complete-project-practice-03/src/App.js
+++ course 2/complete-project-practice-03/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const val = localStorage.getItem("loginValue");
+    if (val === 1) {
+      setIsLoggedIn(1);
+      console.log("here");
+    } else {
+      setIsLoggedIn(0);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem("loginValue", 1);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("loginValue", 0);
+    console.log(localStorage.getItem("loginValue"));
     setIsLoggedIn(false);
   };
 
