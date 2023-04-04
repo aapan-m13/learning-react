e2bb08456b25ba23eb2d0f17f3eb2d1a622547c0
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-04-04 22:31:45 +0600

    functional components

--- component/src/index.js
+++ component/src/index.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import Approval from './Approval';
+import React from "react";
+import ReactDom from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import Approval from "./Approval";
 
-const App = () =>{
-    return (
-        <div className='ui container comments'>
-            <Approval>
-                <div>
-                    <h4>Warning!</h4>
-                    Are you sure?
-                </div>
-            </Approval>
-            <Approval>
-                <CommentDetail author='Aapan' time='Today at 6.00pm' txt='Nice vlog!' imgsrc={faker.image.avatar()}/>
-            </Approval>
-            <Approval>
-                <CommentDetail author='Alex'  time='Yesterday at 1.00pm' txt='I am still dreaming!' imgsrc={faker.image.avatar()}/>
-            </Approval>
-            <Approval>
-                <CommentDetail author='Jane'  time='Yesterday at 8.00am' txt='So, beautiful!' imgsrc={faker.image.avatar()}/>
-            </Approval>
+// functional components
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <Approval>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure?
         </div>
-    );
+      </Approval>
+      <Approval>
+        <CommentDetail
+          author="Aapan"
+          time="Today at 6.00pm"
+          txt="Nice vlog!"
+          imgsrc={faker.image.avatar()}
+        />
+      </Approval>
+      <Approval>
+        <CommentDetail
+          author="Alex"
+          time="Yesterday at 1.00pm"
+          txt="I am still dreaming!"
+          imgsrc={faker.image.avatar()}
+        />
+      </Approval>
+      <Approval>
+        <CommentDetail
+          author="Jane"
+          time="Yesterday at 8.00am"
+          txt="So, beautiful!"
+          imgsrc={faker.image.avatar()}
+        />
+      </Approval>
+    </div>
+  );
 };
 
-
-
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById("root"));
