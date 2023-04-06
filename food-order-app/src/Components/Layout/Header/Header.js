2cab98dccf1d4e81c1f47916c63952c9386cf0ab
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-04-06 17:14:29 +0600

    react app with fundamentals

--- food-order-app/src/Components/Layout/Header/Header.js
+++ food-order-app/src/Components/Layout/Header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import mealsImage from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
 export default function Header(props) {
   return (
     <React.Fragment>
-      <header>
+      <header className={styles["header"]}>
         <h1>Meals</h1>
         <button>Cart</button>
       </header>
-      <div>
+      <div className={styles["main-image"]}>
         <img src={mealsImage} alt="header" />
       </div>
     </React.Fragment>
